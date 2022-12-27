f48789e8fb15be010b7276672fff545892035e5d
SEC <SEC@DESKTOP-7QMB5OD>
2022-12-27 17:31:46 +0900

    [ks45team04 Project] controller author : 황윤아

--- ks45team04/src/main/java/ks45team04/sos/member/controller/PointController.java
+++ ks45team04/src/main/java/ks45team04/sos/member/controller/PointController.java
@@ -1,5 +1,19 @@
 package ks45team04.sos.member.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/point")
 public class PointController {
 
+	//포인트 적립 기준 조회
+		@GetMapping("PointSaveStandardList")
+		public String PointSaveStandardList(Model model) {
+			model.addAttribute("title", "포인트 적립 기준 조회");
+			
+			return "member/point/point_standard_list";
+		}
 }
