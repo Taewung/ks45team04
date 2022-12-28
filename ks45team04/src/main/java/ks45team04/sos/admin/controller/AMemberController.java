3553df34ce524e433943588a564542c31b8d77f6
gkwlw <gkwlw@DESKTOP-KPIJ07J>
2022-12-28 16:10:06 +0900

    [ks45team04 Project] member 폴더 이름 및 경로 수정 author : Taewung

--- ks45team04/src/main/java/ks45team04/sos/admin/controller/AMemberController.java
+++ ks45team04/src/main/java/ks45team04/sos/admin/controller/AMemberController.java
@@ -12,19 +12,19 @@ public class AMemberController {
 	@GetMapping("/memberList")
 	public String memberList(Model model) {
 		model.addAttribute("title", "회원 전체 정보 조회");
-		return  "admin/member/member_list";		
+		return  "admin/adminMember/member_list";		
 	}
 
 	@GetMapping("/memberSavePoint")
 	public String memberSavePoint(Model model) {
 		model.addAttribute("title", "회원가입 포인트 지급");
-		return  "admin/member/member_save_point";		
+		return  "admin/adminMember/member_save_point";		
 	}	
 	
 	@GetMapping("/removeMember")
 	public String removeMember(Model model) {
 		model.addAttribute("title", "회원 삭제");
-		return  "admin/member/remove_member";		
+		return  "admin/adminMember/remove_member";		
 	}		
 	
 }
