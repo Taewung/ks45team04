45a9f65aca64449974b8963b5b146dfbd211e3a2
SEC <SEC@DESKTOP-7QMB5OD>
2023-01-02 09:18:52 +0900

    [ks45team04 Project 포인트,결제,환불 html 수정 및 추가 author : 황윤아

--- ks45team04/src/main/java/ks45team04/sos/member/controller/PayController.java
+++ ks45team04/src/main/java/ks45team04/sos/member/controller/PayController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PayController {
 	
-	//결제(등록)
+	//결제창(등록)
 	@GetMapping("/payInsert")
 	public String payInsert(Model model) {
 		model.addAttribute("title", "결제(등록)");
@@ -16,11 +16,19 @@ public class PayController {
 		return "member/pay/pay_insert";
 	}
 	//결제 내역 조회
-	@GetMapping("/payList")
-	public String payList(Model model) {
+	@GetMapping("/mpayList")
+	public String mpayList(Model model) {
 		model.addAttribute("title", "결제 내역 조회");
 		
-		return "member/pay/pay_list";
+		return "member/pay/mpay_list";
+	}
+	
+	//결제 내역 상세 조회
+	@GetMapping("/payDetail")
+	public String payDetail(Model model) {
+		model.addAttribute("title", "결제 내역 상세 조회");
+		
+		return "member/pay/pay_detail";
 	}
 
 }
