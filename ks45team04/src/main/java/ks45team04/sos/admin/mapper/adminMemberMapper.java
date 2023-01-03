9f7133ba3f8428d63d80aad3bb0cb15e98f35d39
gkwlw <gkwlw@DESKTOP-KPIJ07J>
2023-01-03 14:01:13 +0900

    [ks45team04 Project] 기능 구현 하는중 author : Taewung ks45team04/src/main/java/ks45team04/sos/admin/controller/AMemberController.java ks45team04/src/main/resources/mapper/AMemberMapper.xml ks45team04/src/main/java/ks45team04/sos/admin/dto/Member.java ks45team04/src/main/java/ks45team04/sos/member/dto/Member.java ks45team04/src/main/java/ks45team04/sos/member/mapper/MemberMapper.java ks45team04/src/main/resources/mapper/MemberMapper.xml ks45team04/src/main/java/ks45team04/sos/admin/service/MemberService.java ks45team04/src/main/resources/templates/admin/adminMember/total_member_list.html

---
+++ ks45team04/src/main/java/ks45team04/sos/admin/mapper/adminMemberMapper.java
@@ -0,0 +1,16 @@
+package ks45team04.sos.admin.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import ks45team04.sos.admin.dto.Member;
+
+@Mapper
+public interface adminMemberMapper {
+
+	//전체 회원 정보 조회
+	public List<Member> TotalMemberList();
+	
+	
+}
