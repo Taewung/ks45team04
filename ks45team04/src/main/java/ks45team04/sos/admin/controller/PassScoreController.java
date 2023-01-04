3dc948932d2c1496b79c064bc667e52b7e4c3f82
ECS <ECS@DESKTOP-048HU3O>
2023-01-04 16:29:18 +0900

    [ks45team04 Project] select option 동적 변경
    ks45team04/src/main/resources/templates/admin/passScore/add_license_pass_score.html
    ks45team04/src/main/resources/mapper/ALicensePassScoreMapper.xml
    ks45team04/src/main/java/ks45team04/sos/admin/mapper/LicensePassScoreMapper.java
    ks45team04/src/main/java/ks45team04/sos/admin/service/LicensePassScoreService.java
    ks45team04/src/main/java/ks45team04/sos/admin/controller/PassScoreController.java
    
    자격증별 합격기준점수 등록화면의
    자격증 대분류별 중분류 조회
    author : 김수지

--- ks45team04/src/main/java/ks45team04/sos/admin/controller/PassScoreController.java
+++ ks45team04/src/main/java/ks45team04/sos/admin/controller/PassScoreController.java
@@ -5,10 +5,13 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-
-
+import ks45team04.sos.admin.dto.LicenseMain;
 import ks45team04.sos.admin.dto.LicensePassScore;
+import ks45team04.sos.admin.dto.LicenseSub;
 import ks45team04.sos.admin.service.LicensePassScoreService;
 
 @Controller
@@ -19,24 +22,42 @@ public class PassScoreController {
 	public PassScoreController(LicensePassScoreService licensePassScoreService) {
 		this.licensePassScoreService = licensePassScoreService;
 	}
-
-	@GetMapping("/addLicensePassScore")
-	public String addLicensePassScore(Model model) {
-		model.addAttribute("title", "자격증합격점수등록");
-		return "admin/passScore/add_license_pass_score";
-	}	
+	// 자격증별 합격기준점수 상세정보 조회
 	@GetMapping("/checkLicensePassScore")
 	public String checkLicensePassScore(Model model) {
 		model.addAttribute("title", "자격증합격점수상세조회");
 		return "admin/passScore/check_license_pass_score";
-	}		
+	}	
+	// 자격증별 합격기준점수 수정
 	@GetMapping("/modifyLicensePassScore")
 	public String modifyLicensePassScore(Model model) {
 		model.addAttribute("title", "자격증합격점수수정");
 		return "admin/passScore/modify_license_pass_score";
+	}
+	// 자격증별 합격기준점수 등록
+	@PostMapping("/addLicensePassScore")
+	public String addLicensePassScore(LicensePassScore licensePassScore) {
+		licensePassScoreService.addLicensePassScore(licensePassScore);
+		return "redirect:/licensePassScoreList";		
+	}
+	// 자격증별 합격기준점수 등록
+	@GetMapping("/addLicensePassScore")
+	public String addLicensePassScore(Model model) {
+		List<LicensePassScore> licensePassScoreList = licensePassScoreService.getLicensePassScoreList();
+		List<LicenseMain> licenseMainList = licensePassScoreService.getLicenseMainList();
+		model.addAttribute("licensePassScoreList", licensePassScoreList);
+		model.addAttribute("licenseMainList", licenseMainList);
+		model.addAttribute("title", "자격증합격점수등록");
+		return "admin/passScore/add_license_pass_score";
+	}
+	// 중분류 조회
+	@GetMapping("/getLicenseSubCategory")
+	@ResponseBody
+	public List<LicenseSub> getLicenseSubCategory (@RequestParam(value="lmcCode") String lmcCode){
+		List<LicenseSub> licenseSubList = licensePassScoreService.getLicenseSubList(lmcCode);
+		return licenseSubList;
 	}	
-
-	// 자격증별 합격기준점수 목록
+	// 자격증 합격기준점수목록 조회
 	@GetMapping("/licensePassScoreList")
 	public String licensePassScoreList(Model model) {
 		List<LicensePassScore> licensePassScoreList = licensePassScoreService.getLicensePassScoreList();		
--- ks45team04/src/main/java/ks45team04/sos/admin/service/LicensePassScoreService.java
+++ ks45team04/src/main/java/ks45team04/sos/admin/service/LicensePassScoreService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import ks45team04.sos.admin.dto.LicenseMain;
 import ks45team04.sos.admin.dto.LicensePassScore;
+import ks45team04.sos.admin.dto.LicenseSub;
 import ks45team04.sos.admin.mapper.LicensePassScoreMapper;
 
 
@@ -13,14 +15,40 @@ import ks45team04.sos.admin.mapper.LicensePassScoreMapper;
 public class LicensePassScoreService {
 
 private final LicensePassScoreMapper licensePassScoreMapper;
-
 	public LicensePassScoreService(LicensePassScoreMapper licensePassScoreMapper) {
 		this.licensePassScoreMapper = licensePassScoreMapper;
 	}
 	
-	// 자격증별 합격기준점수 목록 조회
-	public List<LicensePassScore> getLicensePassScoreList(){
+	/**
+	 * 자격증별 합격기준점수 등록
+	 * @param licensePassScore
+	 */
+	public void addLicensePassScore(LicensePassScore licensePassScore) {
+		licensePassScoreMapper.addLicensePassScore(licensePassScore);
+	}
 	
+	/**
+	 * 자격증 대분류별 중분류목록 조회
+	 * @return List<LicenseSub>
+	 */
+	public List<LicenseSub> getLicenseSubList(String lmcCode){
+		List<LicenseSub> licenseSubList = licensePassScoreMapper.getLicenseSubList(lmcCode);
+		return licenseSubList;
+	}	
+	/**
+	 * 자격증 대분류목록 조회
+	 * @return List<LicenseMain>
+	 */
+	public List<LicenseMain> getLicenseMainList(){
+		List<LicenseMain> licenseMainList = licensePassScoreMapper.getLicenseMainList();
+		return licenseMainList;
+	}
+	
+	/**
+	 * 자격증별 합격기준점수 조회
+	 * @return List<LicensePassScore>
+	 */
+	public List<LicensePassScore> getLicensePassScoreList(){	
 		List<LicensePassScore> licensePassScoreList = licensePassScoreMapper.getLicensePassScoreList();	
 		return licensePassScoreList;
 	}
