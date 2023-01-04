2da712d2b74ff0e2dc50041a5a4163d0f348fe4a
ASUS <ASUS@DESKTOP-T50VO8O>
2023-01-04 09:50:09 +0900

    [ks45team04 Project] 전체매출조회 author : 허수지

--- ks45team04/src/main/java/ks45team04/sos/admin/dto/AnnualRefund.java
+++ ks45team04/src/main/java/ks45team04/sos/admin/dto/AnnualRefund.java
@@ -1,5 +1,114 @@
 package ks45team04.sos.admin.dto;
 
 public class AnnualRefund {
-
+	private String annualRefundCode;
+	private String annualRefundYear;
+	private int annualRefundJan;
+	private int annualRefundFeb;
+	private int annualRefundMar;
+	private int annualRefundApr;
+	private int annualRefundMay;
+	private int annualRefundJun;
+	private int annualRefundJul;
+	private int annualRefundAug;
+	private int annualRefundSept;
+	private int annualRefundOct;
+	private int annualRefundNov;
+	private int annualRefundDec;
+	public String getAnnualRefundCode() {
+		return annualRefundCode;
+	}
+	public void setAnnualRefundCode(String annualRefundCode) {
+		this.annualRefundCode = annualRefundCode;
+	}
+	public String getAnnualRefundYear() {
+		return annualRefundYear;
+	}
+	public void setAnnualRefundYear(String annualRefundYear) {
+		this.annualRefundYear = annualRefundYear;
+	}
+	public int getAnnualRefundJan() {
+		return annualRefundJan;
+	}
+	public void setAnnualRefundJan(int annualRefundJan) {
+		this.annualRefundJan = annualRefundJan;
+	}
+	public int getAnnualRefundFeb() {
+		return annualRefundFeb;
+	}
+	public void setAnnualRefundFeb(int annualRefundFeb) {
+		this.annualRefundFeb = annualRefundFeb;
+	}
+	public int getAnnualRefundMar() {
+		return annualRefundMar;
+	}
+	public void setAnnualRefundMar(int annualRefundMar) {
+		this.annualRefundMar = annualRefundMar;
+	}
+	public int getAnnualRefundApr() {
+		return annualRefundApr;
+	}
+	public void setAnnualRefundApr(int annualRefundApr) {
+		this.annualRefundApr = annualRefundApr;
+	}
+	public int getAnnualRefundMay() {
+		return annualRefundMay;
+	}
+	public void setAnnualRefundMay(int annualRefundMay) {
+		this.annualRefundMay = annualRefundMay;
+	}
+	public int getAnnualRefundJun() {
+		return annualRefundJun;
+	}
+	public void setAnnualRefundJun(int annualRefundJun) {
+		this.annualRefundJun = annualRefundJun;
+	}
+	public int getAnnualRefundJul() {
+		return annualRefundJul;
+	}
+	public void setAnnualRefundJul(int annualRefundJul) {
+		this.annualRefundJul = annualRefundJul;
+	}
+	public int getAnnualRefundAug() {
+		return annualRefundAug;
+	}
+	public void setAnnualRefundAug(int annualRefundAug) {
+		this.annualRefundAug = annualRefundAug;
+	}
+	public int getAnnualRefundSept() {
+		return annualRefundSept;
+	}
+	public void setAnnualRefundSept(int annualRefundSept) {
+		this.annualRefundSept = annualRefundSept;
+	}
+	public int getAnnualRefundOct() {
+		return annualRefundOct;
+	}
+	public void setAnnualRefundOct(int annualRefundOct) {
+		this.annualRefundOct = annualRefundOct;
+	}
+	public int getAnnualRefundNov() {
+		return annualRefundNov;
+	}
+	public void setAnnualRefundNov(int annualRefundNov) {
+		this.annualRefundNov = annualRefundNov;
+	}
+	public int getAnnualRefundDec() {
+		return annualRefundDec;
+	}
+	public void setAnnualRefundDec(int annualRefundDec) {
+		this.annualRefundDec = annualRefundDec;
+	}
+	@Override
+	public String toString() {
+		return "AnnualRefund [annualRefundCode=" + annualRefundCode + ", annualRefundYear=" + annualRefundYear
+				+ ", annualRefundJan=" + annualRefundJan + ", annualRefundFeb=" + annualRefundFeb + ", annualRefundMar="
+				+ annualRefundMar + ", annualRefundApr=" + annualRefundApr + ", annualRefundMay=" + annualRefundMay
+				+ ", annualRefundJun=" + annualRefundJun + ", annualRefundJul=" + annualRefundJul + ", annualRefundAug="
+				+ annualRefundAug + ", annualRefundSept=" + annualRefundSept + ", annualRefundOct=" + annualRefundOct
+				+ ", annualRefundNov=" + annualRefundNov + ", annualRefundDec=" + annualRefundDec + "]";
+	}
+	
+	
+	
 }
--- ks45team04/src/main/java/ks45team04/sos/admin/service/SalesService.java
+++ ks45team04/src/main/java/ks45team04/sos/admin/service/SalesService.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import ks45team04.sos.admin.dto.AnnualRefund;
+import ks45team04.sos.admin.dto.AnnualSales;
+import ks45team04.sos.admin.dto.AnnualTotal;
 import ks45team04.sos.admin.dto.DailyRefund;
 import ks45team04.sos.admin.dto.DailySales;
 import ks45team04.sos.admin.dto.DailySalesAndRefund;
 import ks45team04.sos.admin.dto.MonthlySalesAndRefund;
+import ks45team04.sos.admin.dto.MonthlyTotal;
 import ks45team04.sos.admin.mapper.SalesMapper;
 
 @Service
@@ -50,4 +54,44 @@ private final SalesMapper salesMapper;
 			return monthlySalesAndRefundList;
 					
 		}		
+		
+		
+	//월별 손익합계 조회
+		public List<MonthlyTotal> MonthlyTotalList(){
+			 List<MonthlyTotal> monthlyTotalList = salesMapper.MonthlyTotalList();
+					
+			 return monthlyTotalList;
+					
+		}		
+		
+	//년별 매출 조회
+		public List<AnnualSales> AnnualSalesList(){
+			 List<AnnualSales> annualSalesList = salesMapper.AnnualSalesList();
+							
+			 return annualSalesList;
+							
+				}		
+					
+		
+	//년별 환불 조회
+		public List<AnnualRefund> AnnualRefundList(){
+			 List<AnnualRefund> annualRefundList = salesMapper.AnnualRefundList();
+							
+			 return annualRefundList;
+							
+				}			
+		
+	//년별 손익 조회
+		public List<AnnualTotal> AnnualTotalList(){
+			 List<AnnualTotal> annualTotalList = salesMapper.AnnualTotalList();
+								
+			 return annualTotalList;
+								
+				}				
+		
+		
+		
+		
+		
+		
 }
