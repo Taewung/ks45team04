3a7bc4aa97067f3b3903da9f03d1b6742a09d855
gkwlw <gkwlw@193.168.0.50>
2023-01-04 19:48:59 +0900

    [ks45team04 Project] 멤버십 등록 조회, 멤버십 회원 목록 수정 처리 author : Taewung ks45team04/src/main/resources/templates/admin/membership/add_membership.html ks45team04/src/main/resources/templates/admin/fragments/adminAside.html ks45team04/src/main/java/ks45team04/sos/admin/mapper/AdminMembershipMapper.java ks45team04/src/main/java/ks45team04/sos/admin/mapper/AdminMembershipMemberMapper.java ks45team04/src/main/java/ks45team04/sos/admin/controller/AMembershipController.java ks45team04/src/main/resources/mapper/AMembershipMapper.xml ks45team04/src/main/java/ks45team04/sos/admin/controller/AMembershipMemberController.java ks45team04/src/main/resources/mapper/AMembershipMemberMapper.xml ks45team04/src/main/java/ks45team04/sos/admin/dto/Membership.java ks45team04/src/main/java/ks45team04/sos/member/dto/Membership.java ks45team04/src/main/resources/templates/admin/membership/membership_list.html ks45team04/src/main/java/ks45team04/sos/admin/mapper/MembershipMapper.java ks45team04/src/main/java/ks45team04/sos/admin/dto/MembershipMember.java ks45team04/src/main/java/ks45team04/sos/admin/mapper/MembershipMemberMapper.java ks45team04/src/main/java/ks45team04/sos/admin/service/MembershipMemberService.java ks45team04/src/main/java/ks45team04/sos/admin/service/MembershipService.java ks45team04/src/main/resources/templates/admin/membershipMember/total_membership_member_list.html ks45team04/src/main/resources/templates/admin/membershipMember/total_membership_request.html ks45team04/src/main/resources/templates/admin/adminMember/total_remove_member.html  멤버십 등록 조회, 멤버십 회원 목록 수정 처리

---
+++ ks45team04/src/main/java/ks45team04/sos/admin/mapper/AdminMembershipMemberMapper.java
@@ -0,0 +1,14 @@
+package ks45team04.sos.admin.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import ks45team04.sos.admin.dto.MembershipMember;
+
+@Mapper
+public interface AdminMembershipMemberMapper {
+
+		//멤버십 회원 조회
+	public List<MembershipMember> TotalMembershipMemberList();
+}
--- ks45team04/src/main/java/ks45team04/sos/admin/service/MembershipMemberService.java
+++ ks45team04/src/main/java/ks45team04/sos/admin/service/MembershipMemberService.java
@@ -1,5 +1,30 @@
 package ks45team04.sos.admin.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import ks45team04.sos.admin.dto.MembershipMember;
+import ks45team04.sos.admin.mapper.AdminMembershipMemberMapper;
+
+@Service("adminMembershipMemberService")
 public class MembershipMemberService {
+
+private final AdminMembershipMemberMapper adminMembershipMemberMapper;
+
+public MembershipMemberService(AdminMembershipMemberMapper adminMembershipMember) {
+	this.adminMembershipMemberMapper = adminMembershipMember;
+}
+
+/**
+ * 멤버십 회원 조회
+ * @return List<MembershipMember>
+ */
+public List<MembershipMember> TotalMembershipMemberList() {
+	
+	List<MembershipMember> totalMemberhipMemberList = adminMembershipMemberMapper.TotalMembershipMemberList();
+	
+	return totalMemberhipMemberList;
+}
 
 }
