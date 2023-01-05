60be78e25da09370675f067c636af0074ff86f0b
User <User@DESKTOP-JRFET00>
2023-01-05 15:27:27 +0900

    [ks45team04 Project] 관리자페이지 문제목록 조회
    ks45team04/src/main/resources/templates/member/exam/add_exam_answer.html
    ks45team04/src/main/resources/mapper/AQuestionSettingMapper.xml
    ks45team04/src/main/resources/templates/member/layout/contentDefault.html
    ks45team04/src/main/resources/templates/admin/questionSetting/q_setting_list.html
    ks45team04/src/main/java/ks45team04/sos/admin/controller/QuestionSettingController.java
    ks45team04/src/main/java/ks45team04/sos/admin/service/QuestionSettingService.java
    author : 김수지

--- ks45team04/src/main/java/ks45team04/sos/admin/service/QuestionSettingService.java
+++ ks45team04/src/main/java/ks45team04/sos/admin/service/QuestionSettingService.java
@@ -1,7 +1,10 @@
 package ks45team04.sos.admin.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import ks45team04.sos.admin.dto.Question;
 import ks45team04.sos.admin.mapper.QuestionSettingMapper;
 
 @Service
@@ -11,5 +14,14 @@ public class QuestionSettingService {
 	
 	public QuestionSettingService(QuestionSettingMapper questionSettingMapper) {
 		this.questionSettingMapper = questionSettingMapper;
+	}
+	
+	/**
+	 * 문제 정보 목록조회
+	 * @return List<Question>
+	 */
+	public List<Question> getQuestionList(){
+		List<Question> questionList = questionSettingMapper.getQuestionList();
+		return questionList;
 	}
 }
