6ab0035691d08aab76b0745e37d38779cd8120c2
User <User@DESKTOP-JRFET00>
2023-01-06 10:42:47 +0900

    [ks45team04 Project] wrongAnswer/wrongAnswerNote mapper추가
    wrongAnswer/wrongAnswerNote
    mapper.xml 및 mapper.java 생성
    
    ks45team04/src/main/resources/templates/admin/passScore/add_license_pass_score.html
    ks45team04/src/main/resources/mapper/ALicensePassScoreMapper.xml
    ks45team04/src/main/resources/templates/admin/passScore/check_license_pass_score.html
    ks45team04/src/main/java/ks45team04/sos/member/mapper/WrongAnswerMapper.java
    ks45team04/src/main/resources/mapper/WrongAnswerMapper.xml
    ks45team04/src/main/java/ks45team04/sos/member/controller/WrongAnswerNoteController.java
    ks45team04/src/main/java/ks45team04/sos/member/mapper/WrongAnswerNoteMapper.java
    ks45team04/src/main/resources/mapper/WrongAnswerNoteMapper.xml
    ks45team04/src/main/java/ks45team04/sos/member/service/WrongAnswerNoteService.java
    author : 김수지

--- ks45team04/src/main/java/ks45team04/sos/member/mapper/WrongAnswerMapper.java
+++ ks45team04/src/main/java/ks45team04/sos/member/mapper/WrongAnswerMapper.java
@@ -1,5 +1,14 @@
 package ks45team04.sos.member.mapper;
 
-public class WrongAnswerMapper {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
+import ks45team04.sos.member.dto.WrongAnswer;
+
+@Mapper
+public interface WrongAnswerMapper {
+
+	// 오답 목록 조회
+	public List<WrongAnswer> getWrongAnswerList();
 }
