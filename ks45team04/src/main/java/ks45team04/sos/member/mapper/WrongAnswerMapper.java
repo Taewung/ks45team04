f95c6bc8cdb686b7327d5f89f5bc7f0fe034ac56
ECS <ECS@DESKTOP-048HU3O>
2023-01-06 11:53:56 +0900

    [ks45team04 Project] wrongAnswer/wrongAnswerNote.xml 오류 수정
    ks45team04/src/main/java/ks45team04/sos/member/mapper/WrongAnswerMapper.java
    ks45team04/src/main/resources/mapper/WrongAnswerMapper.xml
    ks45team04/src/main/java/ks45team04/sos/member/controller/WrongAnswerNoteController.java
    ks45team04/src/main/java/ks45team04/sos/member/mapper/WrongAnswerNoteMapper.java
    ks45team04/src/main/resources/mapper/WrongAnswerNoteMapper.xml
    author : 김수지

--- ks45team04/src/main/java/ks45team04/sos/member/mapper/WrongAnswerMapper.java
+++ ks45team04/src/main/java/ks45team04/sos/member/mapper/WrongAnswerMapper.java
@@ -1,14 +1,13 @@
 package ks45team04.sos.member.mapper;
 
-import java.util.List;
+
 
 import org.apache.ibatis.annotations.Mapper;
 
-import ks45team04.sos.member.dto.WrongAnswer;
+
 
 @Mapper
 public interface WrongAnswerMapper {
 
-	// 오답 목록 조회
-	public List<WrongAnswer> getWrongAnswerList();
+
 }
--- ks45team04/src/main/java/ks45team04/sos/member/mapper/WrongAnswerNoteMapper.java
+++ ks45team04/src/main/java/ks45team04/sos/member/mapper/WrongAnswerNoteMapper.java
@@ -1,15 +1,14 @@
 package ks45team04.sos.member.mapper;
 
-import java.util.List;
+
 
 import org.apache.ibatis.annotations.Mapper;
 
-import ks45team04.sos.member.dto.WrongAnswerNote;
+
 
 @Mapper
 public interface WrongAnswerNoteMapper {
 	
-	// 오답노트 목록 조회
-	public List<WrongAnswerNote> getWrongAnswerNoteList();
+
 }
  
