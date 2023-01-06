ff33a1e93230df1d307b85503127eafa0f4394cf
ECS <ECS@DESKTOP-048HU3O>
2023-01-06 12:07:56 +0900

    [ks45team04 Project] LicenseInfo DTO 수정
    데이터베이스 컬럼추가로인한 dto수정
    ks45team04/src/main/java/ks45team04/sos/admin/dto/LicenseInfo.java
    author : 김수지

--- ks45team04/src/main/java/ks45team04/sos/admin/dto/LicenseInfo.java
+++ ks45team04/src/main/java/ks45team04/sos/admin/dto/LicenseInfo.java
@@ -2,25 +2,44 @@ package ks45team04.sos.admin.dto;
 
 public class LicenseInfo {
 	private String liCode;
+	private String lmcCode;
+	private String lmcName;
 	private String lscCode;
+	private String lscName;
 	private String liName;
 	private String liOverview;
 	private String liQualification;
 	private String liRegId;
 	private String liRegDatetime;
-	
-	
 	public String getLiCode() {
 		return liCode;
 	}
 	public void setLiCode(String liCode) {
 		this.liCode = liCode;
 	}
+	public String getLmcCode() {
+		return lmcCode;
+	}
+	public void setLmcCode(String lmcCode) {
+		this.lmcCode = lmcCode;
+	}
+	public String getLmcName() {
+		return lmcName;
+	}
+	public void setLmcName(String lmcName) {
+		this.lmcName = lmcName;
+	}
 	public String getLscCode() {
 		return lscCode;
 	}
 	public void setLscCode(String lscCode) {
 		this.lscCode = lscCode;
+	}
+	public String getLscName() {
+		return lscName;
+	}
+	public void setLscName(String lscName) {
+		this.lscName = lscName;
 	}
 	public String getLiName() {
 		return liName;
@@ -52,11 +71,32 @@ public class LicenseInfo {
 	public void setLiRegDatetime(String liRegDatetime) {
 		this.liRegDatetime = liRegDatetime;
 	}
-	
 	@Override
 	public String toString() {
-		return "LicenseInfo [liCode=" + liCode + ", lscCode=" + lscCode + ", liName=" + liName + ", liOverview="
-				+ liOverview + ", liQualification=" + liQualification + ", liRegId=" + liRegId + ", liRegDatetime="
-				+ liRegDatetime + "]";
+		StringBuilder builder = new StringBuilder();
+		builder.append("LicenseInfo [liCode=");
+		builder.append(liCode);
+		builder.append(", lmcCode=");
+		builder.append(lmcCode);
+		builder.append(", lmcName=");
+		builder.append(lmcName);
+		builder.append(", lscCode=");
+		builder.append(lscCode);
+		builder.append(", lscName=");
+		builder.append(lscName);
+		builder.append(", liName=");
+		builder.append(liName);
+		builder.append(", liOverview=");
+		builder.append(liOverview);
+		builder.append(", liQualification=");
+		builder.append(liQualification);
+		builder.append(", liRegId=");
+		builder.append(liRegId);
+		builder.append(", liRegDatetime=");
+		builder.append(liRegDatetime);
+		builder.append("]");
+		return builder.toString();
 	}
+
+	
 }
