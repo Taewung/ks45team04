2064e81f4b93fe48186f2f302a62ecbccb4786b0
User <User@DESKTOP-JRFET00>
2023-01-09 10:27:23 +0900

    [ks45team04 Project] 합격기준점수 등록기능 구현 - 3(등록 및 수정, 검색)
    ks45team04/src/main/resources/templates/member/exam/add_exam_paper.html
    ks45team04/src/main/resources/templates/admin/passScore/add_license_pass_score.html
    ks45team04/src/main/resources/mapper/ALicensePassScoreMapper.xml
    ks45team04/src/main/resources/templates/admin/passScore/check_license_pass_score.html
    ks45team04/src/main/java/ks45team04/sos/member/controller/ExamController.java
    ks45team04/src/main/java/ks45team04/sos/member/mapper/ExamMapper.java
    ks45team04/src/main/resources/mapper/ExamMapper.xml
    ks45team04/src/main/java/ks45team04/sos/member/service/ExamService.java
    ks45team04/src/main/resources/templates/admin/passScore/license_pass_score_list.html
    ks45team04/src/main/java/ks45team04/sos/admin/dto/LicenseInfo.java
    ks45team04/src/main/java/ks45team04/sos/admin/dto/LicensePassScore.java
    ks45team04/src/main/java/ks45team04/sos/admin/mapper/LicensePassScoreMapper.java
    ks45team04/src/main/java/ks45team04/sos/admin/service/LicensePassScoreService.java
    ks45team04/src/main/resources/templates/admin/passScore/modify_license_pass_score.html
    ks45team04/src/main/java/ks45team04/sos/admin/controller/PassScoreController.java
    ks45team04/src/main/java/ks45team04/sos/member/service/WrongAnswerNoteService.java
    author : 김수지

--- ks45team04/src/main/java/ks45team04/sos/member/controller/ExamController.java
+++ ks45team04/src/main/java/ks45team04/sos/member/controller/ExamController.java
@@ -1,22 +1,31 @@
 package ks45team04.sos.member.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import ks45team04.sos.admin.dto.LicenseInfo;
+import ks45team04.sos.admin.dto.LicenseMain;
+import ks45team04.sos.admin.dto.LicenseSub;
+import ks45team04.sos.member.service.ExamService;
 
 @Controller
 public class ExamController {
-
+	
+	private final ExamService examService;
+	
+	public ExamController(ExamService examService) {
+		this.examService = examService;
+	}
+	
 	@GetMapping("/addExamAnswer")
 	public String addExamAnswer(Model model) {
 		model.addAttribute("title", "시험풀이");
 		return "member/exam/add_exam_answer";
-	}
-	@GetMapping("/addExamPaper")
-	public String addExamPaper(Model model) {
-		model.addAttribute("title", "시험지생성");
-		return "member/exam/add_exam_paper";
 	}
 	@GetMapping("/checkExamResult")
 	public String checkExamResultList(Model model) {
@@ -27,5 +36,27 @@ public class ExamController {
 	public String examResultList(Model model) {
 		model.addAttribute("title", "응시결과목록");
 		return "member/exam/exam_result_list";
+	}
+	// 시험지생성화면
+	@GetMapping("/addExamPaper")
+	public String addExamPaper(Model model) {
+		List<LicenseMain> licenseMainList  = examService.getLicenseMainList();
+		model.addAttribute("licenseMainList", licenseMainList);
+		model.addAttribute("title", "시험지생성");
+		return "member/exam/add_exam_paper";
+	}	
+	// 중분류별 자격증목록 조회
+	@GetMapping("/getLicenseCategoryForExam")
+	@ResponseBody
+	public List<LicenseInfo> getLicenseCategory(@RequestParam(value="lscCode") String lscCode){
+		List<LicenseInfo> licenseList =  examService.getLicenseList(lscCode);
+		return licenseList;
+	}	
+	// 대분류별 중분류 조회
+	@GetMapping("/getLicenseSubCategoryForExam")
+	@ResponseBody
+	public List<LicenseSub> getLicenseSubCategory(@RequestParam(value="lmcCode") String lmcCode){
+		List<LicenseSub> licenseSubList = examService.getLicenseSubList(lmcCode);
+		return licenseSubList;
 	}
 }
--- ks45team04/src/main/java/ks45team04/sos/member/service/ExamService.java
+++ ks45team04/src/main/java/ks45team04/sos/member/service/ExamService.java
@@ -1,5 +1,42 @@
 package ks45team04.sos.member.service;
 
-public class ExamService {
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import ks45team04.sos.admin.dto.LicenseInfo;
+import ks45team04.sos.admin.dto.LicenseMain;
+import ks45team04.sos.admin.dto.LicenseSub;
+import ks45team04.sos.member.mapper.ExamMapper;
+
+@Service
+public class ExamService {
+	private final ExamMapper examMapper;
+	public ExamService(ExamMapper examMapper) {
+		this.examMapper = examMapper;
+	}
+	/**
+	 * 자격증 중분류별 자격증목록 조회
+	 * @return List<licenseList>
+	 */
+	public List<LicenseInfo> getLicenseList(String lscCode){
+		List<LicenseInfo> licenseList = examMapper.getLicenseList(lscCode);
+		return licenseList;
+	}	
+	/**
+	 * 자격증 대분류별 중분류목록 조회
+	 * @return List<LicenseSub>
+	 */
+	public List<LicenseSub> getLicenseSubList(String lmcCode){
+		List<LicenseSub> licenseSubList = examMapper.getLicenseSubList(lmcCode);
+		return licenseSubList;
+	}	
+	/**
+	 * 자격증 대분류목록 조회
+	 * @return List<LicenseMain>
+	 */
+	public List<LicenseMain> getLicenseMainList(){
+		List<LicenseMain> licenseMainList = examMapper.getLicenseMainList();
+		return licenseMainList;
+	}	
 }
