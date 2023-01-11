b9e91b8858b24cb867d26e872ac1ee43be01db89
gkwlw <gkwlw@DESKTOP-KPIJ07J>
2023-01-11 10:27:47 +0900

    [ks45team04 Project] 회원가입, 로그인 처리 중 author : Taewung ks45team04/src/main/resources/templates/member/member/add_member.html ks45team04/src/main/resources/templates/member/layout/contentDefault.html ks45team04/src/main/java/ks45team04/sos/member/controller/LoginController.java ks45team04/src/main/java/ks45team04/sos/admin/dto/LoginHistory.java ks45team04/src/main/java/ks45team04/sos/member/dto/LoginHistory.java ks45team04/src/main/java/ks45team04/sos/member/dto/LoginInfo.java ks45team04/src/main/java/ks45team04/sos/member/interceptor/LoginInterceptor.java ks45team04/src/main/java/ks45team04/sos/member/mapper/LoginMemberMapper.java ks45team04/src/main/java/ks45team04/sos/member/controller/MemberController.java ks45team04/src/main/java/ks45team04/sos/member/mapper/MemberMapper.java ks45team04/src/main/resources/mapper/MemberMapper.xml ks45team04/src/main/java/ks45team04/sos/member/dto/MLogin.java ks45team04/src/main/java/ks45team04/sos/member/service/MLoginService.java ks45team04/src/main/java/ks45team04/sos/member/mapper/MMemberMapper.java ks45team04/src/main/java/ks45team04/sos/member/service/MMemberService.java ks45team04/src/main/java/ks45team04/sos/admin/dto/TotalLoginHistory.java

---
+++ ks45team04/src/main/java/ks45team04/sos/member/interceptor/LoginInterceptor.java
@@ -0,0 +1,70 @@
+package ks45team04.sos.member.interceptor;
+
+import java.io.PrintWriter;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.util.WebUtils;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import ks45team04.sos.member.dto.LoginInfo;
+import ks45team04.sos.member.dto.MMember;
+import ks45team04.sos.member.mapper.MemberMapper;
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor{
+	
+	private final MemberMapper memberMapper;
+	
+	public LoginInterceptor (MemberMapper memberMapper) {
+		this.memberMapper = memberMapper;
+	}
+	
+	/**
+	 * HandlerMapping이 핸들러 객체를 결정한 후 HandlerAdapter가 핸들러를 호출하기 전에 호출되는 메소드
+	 * @return true(controller 진입 허용) false (controller 진입 불허) 
+	 */
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		HttpSession session = request.getSession();
+		LoginInfo loginInfo = (LoginInfo) session.getAttribute("S_MEM_INFO");
+		
+		//cookie 하루동안 로그인
+		Cookie cookie = WebUtils.getCookie(request, "loginKeepId");
+		
+		if(cookie != null && loginInfo == null) {
+			String loginKeepId = cookie.getValue();
+			MMember member = memberMapper.getMemInfoById(loginKeepId);
+			if(member != null) {
+				loginInfo = new LoginInfo(loginKeepId
+													, member.getMemName()
+													, member.getMemBirth());
+				session.setAttribute("S_MEM_INFO", loginInfo);
+			}
+		}
+		return false;
+	}
+
+
+
+	public static void msgRedirect(HttpServletResponse response) {
+		try {
+			response.setContentType("text/html; charset=utf-8;");
+			PrintWriter writer = response.getWriter();
+			writer.write("<script>alert('비정상적인 접근입니다.'); location.href='/';</script>");
+			writer.flush();
+			writer.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	
+	
+}
