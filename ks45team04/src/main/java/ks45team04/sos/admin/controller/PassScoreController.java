9cdb28cfcad5921234ebd23daf8722828dcb32c2
User <User@DESKTOP-JRFET00>
2023-01-17 14:05:18 +0900

    [ks45team04 Project] 과목별 합격기준점수 등록기능 구현 - 2
    - lsPScoreCode 자동생성
    - 자격증별 과목합격기준점수 등록을 위한 자격증정보 및 과목정보 조회
    - 자격증별 과목선택시 과목조회 ajax 구현
    
    ks45team04/src/main/resources/templates/admin/passScore/add_subject_pass_score.html
    ks45team04/src/main/resources/mapper/ASubjectPassScoreMapper.xml
    ks45team04/src/main/java/ks45team04/sos/admin/controller/PassScoreController.java
    ks45team04/src/main/java/ks45team04/sos/admin/mapper/SubjectPassScoreMapper.java
    ks45team04/src/main/java/ks45team04/sos/admin/service/SubjectPassScoreService.java
    
    author : 김수지

--- ks45team04/src/main/java/ks45team04/sos/admin/controller/PassScoreController.java
+++ ks45team04/src/main/java/ks45team04/sos/admin/controller/PassScoreController.java
@@ -19,6 +19,7 @@ import ks45team04.sos.admin.dto.LicenseSub;
 import ks45team04.sos.admin.dto.LicenseSubject;
 import ks45team04.sos.admin.dto.SubjectPassScore;
 import ks45team04.sos.admin.service.LicensePassScoreService;
+import ks45team04.sos.admin.service.LicenseSubjectService;
 import ks45team04.sos.admin.service.SubjectPassScoreService;
 
 @Controller
@@ -54,47 +55,50 @@ public class PassScoreController {
 		return "redirect:/subjectPassScoreList?liCode=${liCode}";
 	}
 	// 과목합격기준점수 등록처리
-	
-	// 특정 과목정보 조회
-	@GetMapping("/getSubInfoForLsPScore")
-	@ResponseBody
-	public LicenseSubject getSubInfoForLsPScore (@RequestParam(value="lsCode") String lsCode){
-		LicenseSubject subInfoForLsPScore = subjectPassScoreService.getSubInfoForLsPScore(lsCode);
-		log.info("특정 과목정보 조회 : {}", subInfoForLsPScore);
-		return subInfoForLsPScore;
-	}	
-	// 자격증별 과목목록 조회
-	@GetMapping("/getSubListForLsPScore")
-	@ResponseBody
-	public List<LicenseSubject> getSubListForLsPScore (@RequestParam(value="liCode") String liCode){
-		List<LicenseSubject> subListForLsPScore = subjectPassScoreService.getSubListForLsPScore(liCode);	
-		log.info("자격증별 과목목록 조회 : {}", subListForLsPScore);
-		return subListForLsPScore;
+	@PostMapping("/addSubjectPassScore")
+	public String addSubjectPassScore(SubjectPassScore subjectPassScore) {
+		// lsPScoreCode 자동생성
+		String newLsPScoreCode = subjectPassScoreService.getNewLsPScoreCode("subject_pass_score", "ls_p_score_code");
+		subjectPassScore.setLsPScoreRegId("id002");
+		subjectPassScore.setLsPScoreCode(newLsPScoreCode);
+		subjectPassScoreService.addSubjectPassScore(subjectPassScore);
+		log.info("과목합격기준점수등록 쿼리파라미터: {}", subjectPassScore);
+		String liCode = subjectPassScore.getLiCode();
+		return "redirect:/subjectPassScoreList?liCode="+liCode;
 	}
 	// 과목합격기준점수 등록화면
 	@GetMapping("/addSubjectPassScore")
 	public String addSubjectPassScore(Model model, @RequestParam(value="liCode", required=false) String liCode) {
 		LicensePassScore liInfoForLsPScore = subjectPassScoreService.getLiInfoForLsPScore(liCode);
+		List<LicenseSubject> liSubListForLsPScore = subjectPassScoreService.getLiSubListForLsPScore(liCode);
+		model.addAttribute("liSubListForLsPScore", liSubListForLsPScore);
 		model.addAttribute("liInfoForLsPScore", liInfoForLsPScore);
 		model.addAttribute("title", "합격기준점수 등록");
 		log.info("특정 자격증정보 조회 : {}", liInfoForLsPScore);	
+		log.info("특정 자격증 과목목록 조회 : {}", liSubListForLsPScore);	
 		
 		return "admin/passScore/add_subject_pass_score";
+	}
+	// 특정 자격증 과목정보 조회
+	@GetMapping("/getLsInfoForLsPScore")
+	@ResponseBody
+	public LicenseSubject getLsInfoForLsPScore(@RequestParam(value="lsCode") String lsCode) {
+		LicenseSubject lsInfoForLsPScore = subjectPassScoreService.getLsInfoForLsPScore(lsCode);
+		log.info("특정 과목정보 조회 : {}", lsInfoForLsPScore);
+		return lsInfoForLsPScore;
 	}
 	// 자격증별 과목합격기준점수목록 조회
 	@GetMapping("/subjectPassScoreList")
 	public String getsubjectPassScoreList(Model model, @RequestParam(value="liCode", required=false) String liCode) {
-	List<SubjectPassScore> subjectPassScoreList = subjectPassScoreService.getsubjectPassScoreList(liCode); 
-	SubjectPassScore liCodeForLsPScore = subjectPassScoreService.getLiCodeForLsPScore(liCode);
-	model.addAttribute("liCodeForLsPScore", liCodeForLsPScore);
-	model.addAttribute("subjectPassScoreList", subjectPassScoreList);
-	model.addAttribute("title", "과목별 합격기준점수 목록");	
-	log.info("자격증정보 : {}", liCodeForLsPScore);
-	log.info("과목합격기준점수목록 : {}", liCode);
+		List<SubjectPassScore> subjectPassScoreList = subjectPassScoreService.getsubjectPassScoreList(liCode); 
+		SubjectPassScore liCodeForLsPScore = subjectPassScoreService.getLiCodeForLsPScore(liCode);
+		model.addAttribute("liCodeForLsPScore", liCodeForLsPScore);
+		model.addAttribute("subjectPassScoreList", subjectPassScoreList);
+		model.addAttribute("title", "과목별 합격기준점수 목록");	
+		log.info("자격증정보 : {}", liCodeForLsPScore);
+		log.info("과목합격기준점수목록 : {}", liCode);
 	return "admin/passScore/subject_pass_score_list";
 	}
-	
-
 	// 자격증별 합격기준점수 상세정보 조회
 	@GetMapping("/checkLicensePassScore")
 	public String checkLicensePassScore(@RequestParam(value="liPScoreCode", required=false) String liPScoreCode
